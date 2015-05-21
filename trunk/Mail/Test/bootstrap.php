50fa9d03661e62f2cc33746274c4ae4a2a7bd36b
christian.wuerker <christian.wuerker@06fe172c-8d1c-df84-e3db-49cf70e4a585>
2015-05-21 17:29:47 +0000

    Mail: Updated make file and test bootstrap.

--- trunk/Mail/Test/bootstrap.php
+++ trunk/Mail/Test/bootstrap.php
@@ -9,7 +9,8 @@ require_once 'Part/Abstract.php5';
 require_once 'Part/Attachment.php5';
 require_once 'Part/Text.php5';
 require_once 'Part/HTML.php5';
-
+require_once 'Check/Address.php5';
+require_once 'Check/Recipient.php5';
 
 class Test_Case extends PHPUnit_Framework_TestCase{}
 
