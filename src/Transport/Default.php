8c02dc23c0f766654a0f0f2077b69681f730f744
Christian Würker <kriss@ceusmedia.de>
2015-06-08 10:37:38 +0200

    Fix @package in code doc blocks.

--- src/Transport/Default.php
+++ src/Transport/Default.php
@@ -18,7 +18,7 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *	@category		Library
- *	@package		CeusMedia_Mail
+ *	@package		CeusMedia_Mail_Transport
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  *	@copyright		2007-2015 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
@@ -28,7 +28,7 @@ namespace CeusMedia\Mail\Transport;
 /**
  *	Sends Mails of different Types.
  *	@category		Library
- *	@package		CeusMedia_Mail
+ *	@package		CeusMedia_Mail_Transport
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  *	@copyright		2007-2015 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
