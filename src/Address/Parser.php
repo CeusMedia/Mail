cc479222fb564cac91839c35450cebb3de94f4b5
Christian Würker <kriss@ceusmedia.de>
2017-05-11 20:06:07 +0200

    Allow third syntax pattern in address parser.

--- src/Address/Parser.php
+++ src/Address/Parser.php
@@ -5,6 +5,7 @@ class Parser{
 	/**	@var	array		$patterns		Map of understandable patterns (regular expressions) */
 	static protected $patterns	= array(												//  define name patterns
 		'name <local-part@domain>'	=> "/^(.*)\s(<((\S+)@(\S+))>)$/U",					//  full address: name and local-part at domain with (maybe in brackets)
+		'<local-part@domain>'		=> "/^<((\S+)@(\S+))>$/U",							//  short address: local-part at domain without name (and no brackets)
 		'local-part@domain'			=> "/^((\S+)@(\S+))$/U",							//  short address: local-part at domain without name (and no brackets)
 	);
 
@@ -20,7 +21,8 @@ class Parser{
 		$string		= stripslashes( trim( $string ) );
 		$string		= preg_replace( "/\r\n /", " ", $string );							//  unfold @see http://tools.ietf.org/html/rfc822#section-3.1
 		$regex1		= self::$patterns['name <local-part@domain>'];						//  get pattern of full address
-		$regex2		= self::$patterns['local-part@domain'];								//  get pattern of short address
+		$regex2		= self::$patterns['<local-part@domain>'];							//  get pattern of short address
+		$regex3		= self::$patterns['local-part@domain'];								//  get pattern of short address
 		if( preg_match( $regex1, $string ) ){											//  found full address: with name or in brackets
 			$localPart	= preg_replace( $regex1, "\\4", $string );						//  extract local part
 			$domain		= preg_replace( $regex1, "\\5", $string );						//  extract domain part
@@ -32,16 +34,20 @@ class Parser{
 			$domain		= preg_replace( $regex2, "\\3", $string );						//  extract domain part
 			$name		= NULL;															//  clear user name
 		}
+		else if( preg_match( $regex3, $string ) ){										//  otherwise found short address: neither name nor brackets
+			$localPart	= preg_replace( $regex3, "\\2", $string );						//  extract local part
+			$domain		= preg_replace( $regex3, "\\3", $string );						//  extract domain part
+			$name		= NULL;															//  clear user name
+		}
 		else{																			//  not matching any pattern
 			$list		= '"'.implode( '" or "', array_keys( self::$patterns ) ).'"';
 			$message	= 'Invalid address given (must match '.$list.')' ;
 			throw new \InvalidArgumentException( $message );
 		}
-		return (object) array(
-			'name'		=> $name,
-			'address'	=> $localPart.'@'.$domain,
-			'localPart'	=> $localPart,
-			'domain'	=> $domain,
-		);
+		$address	= new \CeusMedia\Mail\Address();
+		$address->setDomain( $domain );
+		$address->setLocalPart( $localPart );
+		$address->setName( $name );
+		return $address;
 	}
 }
