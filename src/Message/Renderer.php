f1d1bfee9aef2d66f71fa325692dda3fa7c8cb34
kriss0r <kriss@ceusmedia.de>
2018-02-12 19:17:38 +0100

    Render message with only 1 part not as multipart.

--- src/Message/Renderer.php
+++ src/Message/Renderer.php
@@ -37,66 +37,55 @@ namespace CeusMedia\Mail\Message;
  */
 class Renderer{
 
-	static protected $userAgent		= "CeusMedia/Mail/2.0";
-
-	/**
-	 *	Get set mail agent.
-	 *	@static
-	 *	@access		public
-	 *	@return		string		Mailer user agent
-	 *	@deprecated	use Message::getUserAgent instead
-	 *	@todo    	to be removed in 1.2
-	 */
-	static public function getAgent(){
-		trigger_error( 'Use Message::getUserAgent instead', E_USER_DEPRECATED );
-		return self::$userAgent;
-	}
-
 	static public function render( \CeusMedia\Mail\Message $message ){
-		if( !count( $message->getParts() ) )
+		if( !count( $message->getParts( TRUE ) ) )
 			throw new \RuntimeException( "No content part set" );
 		if( !strlen( trim( $message->getSubject() ) ) )
 			throw new \RuntimeException( "No subject set" );
 
-		$delim			= \CeusMedia\Mail\Message::$delimiter;
-		$mimeBoundary	= "------".md5( microtime( TRUE ) );
-		$mimeBoundary1	= "------".md5( microtime( TRUE ) + 1 );
-
-		$server		= empty( $_SERVER['SERVER_NAME'] ) ? 'localhost' : $_SERVER['SERVER_NAME'];
-
 		$headers	= $message->getHeaders();
-		$headers->setFieldPair( 'Message-ID', "<".sha1( microtime() )."@".$server.">" );
-		$headers->setFieldPair( 'Date', date( "D, d M Y H:i:s O", time() ) );
+		if( !$headers->hasField( 'Message-ID' ) ){
+			$server	= empty( $_SERVER['SERVER_NAME'] ) ? 'localhost' : $_SERVER['SERVER_NAME'];
+			$headers->setFieldPair( 'Message-ID', "<".sha1( microtime() )."@".$server.">" );
+		}
+		if( !$headers->hasField( 'Date' ) )
+			$headers->setFieldPair( 'Date', date( "D, d M Y H:i:s O", time() ) );
 		$headers->setFieldPair( 'Subject', $message->getSubject( 'base64' ) );
-		$headers->setFieldPair( 'Content-Type', 'multipart/related;'.$delim.' boundary="'.$mimeBoundary.'"' );
 		$headers->setFieldPair( 'MIME-Version', '1.0' );
 		$headers->addFieldPair( 'X-Mailer', $message->getUserAgent() );
 
-		$contents	= array( "This is a multi-part message in MIME format." );
-		$contents[]	= "--".$mimeBoundary;
-		$contents[]	= 'Content-Type: multipart/alternative; boundary="'.$mimeBoundary1.'"';
-		$contents[]	= "";
-		foreach( $message->getParts() as $part )
-			$contents[]	= "--".$mimeBoundary1.$delim.rtrim( $part->render() ).$delim;
-		$contents[]	= "--".$mimeBoundary1."--".$delim;
-		foreach( $message->getAttachments() as $part )
-			$contents[]	= "--".$mimeBoundary.$delim.rtrim( $part->render() ).$delim;
-		$contents[]	= "--".$mimeBoundary."--".$delim;
-		return $headers->toString().$delim.$delim.join( $delim, $contents );
-	}
+		if( count( $message->getParts( TRUE ) ) === 1 ){						//  no multipart message
+			$parts	= $message->getParts( TRUE );								//  get parts
+			$part	= array_pop( $parts );										//  get solo part
+			return $part->render( $headers );									//  render part and apply part headers as message headers
+		}
 
-	/**
-	 *	Sets mail agent for mailer header.
-	 *	@static
-	 *	@access		public
-	 *	@param		string		$userAgent		Mailer user agent
-	 *	@return		void
-	 *	@deprecated	use Message::setUserAgent instead
-	 *	@todo    	to be removed in 1.2
-	 */
-	static public function setAgent( $userAgent ){
-		trigger_error( 'Use Message::setUserAgent instead', E_USER_DEPRECATED );
-		self::$userAgent = $userAgent;
+		$delim			= \CeusMedia\Mail\Message::$delimiter;
+		$mimeBoundary	= "------".md5( microtime( TRUE ) );					//  main multipart boundary
+		$mimeBoundary1	= "------".md5( microtime( TRUE ) + 1 );				//  nested multipart boundary
+		if( count( $message->getParts() ) > 1 ){								//  alternative content parts
+			$headers->setFieldPair( 'Content-Type', 'multipart/related;'.$delim.' boundary="'.$mimeBoundary.'"' );
+			$contents	= array( "This is a multi-part message in MIME format." );
+			$contents[]	= "--".$mimeBoundary;
+			$contents[]	= 'Content-Type: multipart/alternative; boundary="'.$mimeBoundary1.'"';
+			$contents[]	= "";
+			foreach( $message->getParts() as $part )
+				$contents[]	= "--".$mimeBoundary1.$delim.rtrim( $part->render() ).$delim;
+			$contents[]	= "--".$mimeBoundary1."--".$delim;
+			foreach( $message->getAttachments() as $part )
+				$contents[]	= "--".$mimeBoundary.$delim.rtrim( $part->render() ).$delim;
+			$contents[]	= "--".$mimeBoundary."--".$delim;
+		}
+		else{
+			$headers->setFieldPair( 'Content-Type', 'multipart/mixed;'.$delim.' boundary="'.$mimeBoundary.'"' );
+			$contents	= array( "This is a multi-part message in MIME format." );
+			foreach( $message->getParts() as $part )
+				$contents[]	= "--".$mimeBoundary.$delim.rtrim( $part->render() ).$delim;
+			foreach( $message->getAttachments() as $part )
+				$contents[]	= "--".$mimeBoundary.$delim.rtrim( $part->render() ).$delim;
+			$contents[]	= "--".$mimeBoundary."--".$delim;
+		}
+		return $headers->toString( TRUE ).$delim.$delim.join( $delim, $contents );
 	}
 }
 ?>
