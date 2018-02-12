623aa093a889b0f48de9ce6dfd309649da7e8655
kriss0r <kriss@ceusmedia.de>
2018-02-12 19:18:15 +0100

    Rewrite parser to support not multipart messages and header field folding.

--- src/Message/Parser.php
+++ src/Message/Parser.php
@@ -38,191 +38,121 @@ namespace CeusMedia\Mail\Message;
  */
 class Parser{
 
-	static public function parse( $content ){
-		$mail	= new \CeusMedia\Mail\Message();
+	static protected function getCharsetFromContentType( $contentType ){
+		$parts	= explode( ";", $contentType );
 		foreach( $parts as $part ){
-			$mail->addPart( $part );
+			$pair	= explode( "=", trim( $part ) );
+			if( $pair[0] === "charset" )
+				return trim( $pair[1] );
 		}
-		return $mail;
+		return NULL;
 	}
 
-	/**
-	 *	Read part and return a mail part object.
-	 *	@static
-	 *	@access		protected
-	 * 	@param		object		$part			Parsed body part data object
-	 *	@param		array		$options		Map of options (mimeType, format, encoding, charset)
-	 *	@return		object
-	 *	@todo		finish: return an object of \CeusMedia\Mail\Message\Part
-	 *	@todo		implement attachments
-	 */
-	static protected function getPartObject( $part, $options = array(), $verbose = NULL ){
-		$object		= (object) array(																//  prepare body part data object
-			'headers'	=> array(),																	//  ... with empty header list
-			'content'	=> $part->content,															//  ... with raw content
-			'body'		=> $part->content,															//  ... with body (filtered later)
-			'mimeType'	=> isset( $options['mimeType'] ) ? $options['mimeType'] : 'text/plain',		//  ... with MIME type
-			'format'	=> isset( $options['format'] ) ? $options['format'] : NULL,					//  ... with format
-			'encoding'	=> isset( $options['encoding'] ) ? $options['encoding'] : NULL,				//  ... with encoding
-			'charset'	=> isset( $options['charset'] ) ? $options['charset'] : 'UTF-8',			//  ... with charset
-		);
-		$headers	= array();																		//  prepare list for normalized headers
-		$buffer		= "";																			//  prepare empty buffer
-		foreach( $part->headers as $header ){														//  iterate part headers
-			if( preg_match( "/;\s*$/", $header ) )													//  header is wrapped
-				$buffer	.= $header;																	//  store header part in buffer
-			else{																					//  complete header or completing line
-				$headers[]	= $buffer.$header;														//  collect normalized header
-				$buffer		= "";																	//  clear buffer
+	static protected function getMimeFromContentType( $contentType ){
+		$parts	= explode( ";", $contentType );
+		return trim( $parts[0] );
+	}
+
+	static public function parse( $content ){
+		$message	= new \CeusMedia\Mail\Message();
+		$parts		= preg_split( "/\r?\n\r?\n/", $content, 2 );
+
+		$headers	= \CeusMedia\Mail\Message\Header\Parser::parse( $parts[0] );
+		foreach( $headers->getFields() as $field ){
+			$message->addHeader( $field );
+			switch( $field->getName() ){
+				case 'Subject':
+					$message->setSubject( $field->getValue() );
+					break;
 			}
 		}
-		foreach( $headers as $header ){																//  iterate normalized headers
-			$headerParts	= preg_split( "/\s*:\s*/", $header, 2 );								//  split header assignment
-			if( count( $headerParts ) === 2 ){														//  is header assignment
-				$object->headers[$headerParts[0]]	= $headerParts[1];								//  enlist header pair
-				switch( strtolower( $headerParts[0] ) ){
-					case 'content-type':															//  found content type
-						$valueParts	= preg_split( "/\s*;\s*/", $headerParts[1] );					//  may be several information
-						$object->mimeType	= trim( array_shift( $valueParts ) );					//  first part is MIME type
-						foreach( $valueParts as $valuePart ){										//  iterate additional parts
-							$valuePartParts	= preg_split( "/\s*=\s*/", trim( $valuePart ), 2 );		//  split value part assignment
-							if( count( $valuePartParts ) > 1 )										//  is value part assignment
-								$object->$valuePartParts[0]	= $valuePartParts[1];					//  note as part parameter
-						}
-						break;
-					case 'content-transfer-encoding':												//  found transfer encoding
-						$object->encoding	= trim( $headerParts[1] );								//  note as part parameter
-						break;
+		$contentType	= $headers->getField( 'Content-Type' )->getValue();
+		if( preg_match( "/multipart/", $contentType ) ){						//  is multipart message
+			self::parseMultipartBody( $message, $content );						//  parse multipart containers
+		}
+		else{
+			$message->addPart( self::parseAtomicBodyPart( $content ) );			//  directly parse mail content as part
+		}
+		return $message;
+	}
+
+	static protected function parseMultipartBody( $message, $content ){
+		$delim		= \CeusMedia\Mail\Message::$delimiter;
+		$parts		= preg_split( "/\r?\n\r?\n/", $content, 2 );
+		$headers	= \CeusMedia\Mail\Message\Header\Parser::parse( $parts[0] );
+		$body		= $parts[1];
+
+		$contentType	= $headers->getField( 'Content-Type' );
+		if( preg_match( '/boundary/', $contentType ) ){
+			$mimeBoundary	= preg_replace( "/^.+boundary=\"(.+)\"$/", "\\1", $contentType );
+			$lines	= array();
+			$status	= 0;
+			foreach( preg_split( "/\r?\n/", $body ) as $nr => $line ){
+				if( $line === '--'.$mimeBoundary ){
+					if( $status === 0 ){
+						$status	= 1;
+						continue;
+					}
+					self::parseMultipartBody( $message, join( $delim, $lines ) );
+					$lines	= array();
 				}
+				else if( $line === '--'.$mimeBoundary.'--' ){
+					self::parseMultipartBody( $message, join( $delim, $lines ) );
+					break;
+				}
+				else if( $status === 1 )
+					$lines[]	= $line;
 			}
-
 		}
-		$verbose ? remark( "Headers: ") : NULL;
-		$verbose ? print_m( $object->headers ) : NULL;
+		else{
+			$part	= self::parseAtomicBodyPart( $content );
+			$message->addPart( $part );
+		}
+	}
 
-		if( strtolower( $object->format === "fixed" ) )
-			$object->body	= join( $object->body );
-		else
-			$object->body	= join( "\n", $object->body );
-		if( strtolower( $object->encoding ) === "base64" )
-			$object->body	= base64_decode( $object->body );
+	static protected function parseAtomicBodyPart( $content ){
+		$parts		= preg_split( "/\r?\n\r?\n/", $content, 2 );
+		$headers	= \CeusMedia\Mail\Message\Header\Parser::parse( $parts[0] );
+		$content	= $parts[1];
 
+		$contentType	= $headers->getField( 'Content-Type' )->getValue();
+		$mimeType		= self::getMimeFromContentType( $contentType );
+		$charset		= self::getCharsetFromContentType( $contentType );
 
-		if( isset( $object->headers['Content-Disposition'] ) ){
-			if( preg_match( "/attachment/", $object->headers['Content-Disposition'] ) ){
+		if( $headers->hasField( 'Content-Disposition' ) ){
+			if( preg_match( "/attachment/", $headers->getField( 'Content-Disposition' )->getValue() ) ){
 				$part	= new \CeusMedia\Mail\Message\Part\Attachment();
-				$part->setContent( $object->body, $object->headers['Content-Type'] );
-				if( $object->encoding )
-					$part->setEncoding( $object->encoding );
-				if( $object->format )
-					$part->setFormat( $object->format );
-				if( isset( $object->headers['Content-Description'] ) )
-					$filename	= $object->headers['Content-Description'];
+				$part->setContent( $content, $contentType );
+				if( $headers->hasField( 'Content-Transfer-Encoding' ) )
+					$part->setEncoding( $headers->getField( 'Content-Transfer-Encoding' )->getValue() );
+//				if( $object->format )
+//					$part->setFormat( $object->format );
+				if( $headers->hasField( 'Content-Description' ) )
+					$filename	= $headers->getField( 'Content-Description' );
 				else
-					$filename	= preg_replace( "/^.+filename=\"(.+)\"$/", "\\1", $object->headers['Content-Disposition'] );
+					$filename	= preg_replace( "/^.+filename=\"(.+)\"$/", "\\1", $headers->getField( 'Content-Disposition' )->getValue() );
 				if( $filename )
 					$part->setFilename( $filename );
 				return $part;
 			}
 		}
-
-		switch( strtolower( $object->mimeType ) ){
+		switch( strtolower( $mimeType ) ){
 			case 'text/html':
-				$part	= new \CeusMedia\Mail\Message\Part\HTML( $object->body, $object->charset );
-				if( $object->encoding )
-					$part->setEncoding( $object->encoding );
-				if( $object->format )
-					$part->setFormat( $object->format );
+				$part	= new \CeusMedia\Mail\Message\Part\HTML( $content, $charset );
+				if( $headers->hasField( 'Content-Transfer-Encoding' ) )
+					$part->setEncoding( $headers->getField( 'Content-Transfer-Encoding' )->getValue() );
+//				if( $object->format )
+//					$part->setFormat( $object->format );
 				return $part;
 			case 'text/plain':
 			default:
-				$part	= new \CeusMedia\Mail\Message\Part\Text( $object->body, $object->charset );
-				if( $object->encoding )
-					$part->setEncoding( $object->encoding );
-				if( $object->format )
-					$part->setFormat( $object->format );
+				$part	= new \CeusMedia\Mail\Message\Part\Text( $content, $charset );
+				if( $headers->hasField( 'Content-Transfer-Encoding' ) )
+					$part->setEncoding( $headers->getField( 'Content-Transfer-Encoding' )->getValue() );
+//				if( $object->format )
+//					$part->setFormat( $object->format );
 				return $part;
 		}
-	}
-
-	/**
-	 *	Parse mail body and return found body parts.
-	 *	@static
-	 *	@access		public
-	 *	@param		string		$body			Mail body content
-	 *	@return		array		List of found body parts
-	 */
-	static public function parseBody( $body ){
-		$list	= array();																			//  prepare flat body parts list
-		$lines	= preg_split( "/\r?\n/", $body );													//  split body lines
-		$parts	= array();
-		self::parseBodyPart( $parts, $lines, 1 );													//  parse body parts recursively
-		foreach( $parts as $partKey => $subparts ){													//  iterate body part lists
-			foreach( $subparts as $nr => $entry ){													//  iterate body parts
-				$list[$partKey.'-'.$nr]	= self::getPartObject( $entry );							//  ...
-			}
-		}
-		return $list;																				//  return parts collected from body
-	}
-
-	/**
-	 *	Parse body part and nested parts recursively.
-	 *	@static
-	 *	@access		protected
-	 *	@param		array		$lines			List of body part lines
-	 *	@param		integer		$initialStatus	Status to start at (0: read header, 1: read content)
-	 *	@return		object		Body part data object
-	 */
-	static protected function parseBodyPart( &$parts, $lines, $initialStatus = 0, $verbose = FALSE ){
-		$verbose ? remark( "0: Lines: ".count( $lines ) ) : NULL;
-		$status	= max( 0, min( 1, $initialStatus ) );												//  prepare mode within 0 and 1
-		$part	= (object) array(																	//  prepare part data object
-			'headers'	=> array(),																	//  ... with headers list
-			'content'	=> array(),																	//  ... and content
-			'nested'	=> array(),																	//  ... and list of nested parts
-		);
-		foreach( $lines as $line ){																	//  iterate lines
-			if( $status === 0 ){																	//  in mode: read header
-				if( preg_match( "/^$/", $line ) ){													//  found empty line after headers
-					$verbose ? remark( "0: Found ".count( $part->headers )." headers" ) : NULL;
-					$status = 1;																	//  switch to mode: read content
-					continue;																		//  go to next line
-				}
-				$part->headers[]	= $line;														//  otherwise collect header line
-			}
-			else if( $status === 1 ){																//  in mode: read content
-				if( preg_match( "/^--(\S+)$/", $line ) ){											//  found boundary start
-					$subpartKey		= preg_replace( "/^--(\S+)$/", "\\1", $line );					//  get subpart boundary key
-					$verbose ? remark( "1: Found start of ".$subpartKey ) : NULL;
-					$subpartLines	= array();														//  start subpart lines buffer
-					$status			= 2;															//  switch to mode: read subpart
-					$parts[$subpartKey]	= array();
-					continue;																		//  go to next line
-				}
-				$part->content[]	= $line;														//  otherwise collect content line
-			}
-			else if( $status === 2 ){																//  in mode: read subpart
-				if( preg_match( "/^--".preg_quote( $subpartKey, "/" )."$/", $line ) ){				//  found boundary AGAIN
-					$verbose ? remark( "2: Found part with ".count( $subpartLines )." lines" ) : NULL;
-					$verbose ? print_m( array_slice( $subpartLines, 0, 30 ) ) : NULL;
-					$parts[$subpartKey][]	= self::parseBodyPart( $parts, $subpartLines, $verbose );			//  parse nested body part
-					$subpartLines	= array();														//  start subpart lines buffer
-					$status			= 2;															//  switch to mode: read subpart
-					continue;																		//  go to next line
-				}
-				if( preg_match( "/^--".preg_quote( $subpartKey, "/" )."--$/", $line ) ){			//  found boundary end
-					$verbose ? remark( "2: Found end of ".$subpartKey ) : NULL;
-					$verbose ? remark( "2: Found LAST part with ".count( $subpartLines )." lines" ) : NULL;
-					$verbose ? print_m( array_slice( $subpartLines, 0, 30 ) ) : NULL;
-					$parts[$subpartKey][]	= self::parseBodyPart( $parts, $subpartLines, $verbose );			//  parse nested body part
-					$status		= 3;																//  switch to mode: done
-					continue;																		//  go to next line
-				}
-				$subpartLines[]	= $line;															//  otherwise collect subpart line
-			}
-		}
-		$verbose ? remark( "X: Return part with ".count( $part->content )." lines" ) : NULL;
-		return $part;																				//  return this body part
 	}
 }
 ?>
