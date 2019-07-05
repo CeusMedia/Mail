91cb84d20cbc314fd6227a9aa5cae9793905cbc6
Christian Würker <kriss@ceusmedia.de>
2019-07-06 00:06:49 +0200

    Add test with real mail sending and receiving.

--- test/Transport/SMTPTest.php
+++ test/Transport/SMTPTest.php
@@ -5,6 +5,11 @@
  *	@package		CeusMedia_Mail
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  */
+
+use \CeusMedia\Mail\Message;
+use \CeusMedia\Mail\Address;
+use \CeusMedia\Mail\Mailbox;
+
 require_once dirname( __DIR__ ).'/bootstrap.php';
 /**
  *	Unit test for mail address.
@@ -14,16 +19,19 @@ require_once dirname( __DIR__ ).'/bootstrap.php';
  */
 class Transport_SMTPTest extends TestCase
 {
-	public function testSend(){
-		$message	= \CeusMedia\Mail\Message::create()
+	protected $receiveLoopSleep			= 1;
+	protected $receiveLoopTimeout		= 120;
+
+	public function testSend_Mocked(){
+		$message	= Message::create()
 			->addRecipient( 'receiver@muster-server.tld' )
 			->setSender( 'sender@muster-server.tld' )
-			->addText( 'Das ist ein <b>Test</b>.' )
-			->setSubject( 'Das ist ein Test' );
+			->addText( 'This is a <b>test</b>.' )
+			->setSubject( 'This is a test' );
 
 //		print( \CeusMedia\Mail\Message\Renderer::render( $message ) );
 
-		$smtp		= new \CeusMedia\Mail\Transport\SMTP( 'egal', 1 );			//  get SMTP instance
+		$smtp		= new \CeusMedia\Mail\Transport\SMTP( '_not_relevant_' );	//  get SMTP instance
 		$socket		= new SmtpSocketMock();										//  create mocking replacement for default SMTP socket
 		$smtp->setSocket( $socket );											//  set mocking SMTP socket
 		$smtp->setAuth( 'username', 'password' );								//  auth is supported by mock
@@ -36,10 +44,145 @@ class Transport_SMTPTest extends TestCase
 			$creation	= $returned instanceof \CeusMedia\Mail\Transport\SMTP;
 			$this->assertTrue( $creation );
 		}
-		catch( Exception $e ){
+		catch( \Exception $e ){
 //			print_r( $socket->getLog() );
 			$this->fail( $e );
 		}
+	}
+
+	public function testSend(){
+		$configSender		= $this->getSenderConfig();
+		$configReceiver		= $this->getReceiverConfig();
+
+		$subject	= 'Test-Automation-Message #'.\Alg_ID::uuid();
+
+		/*  --  SENDING  --  */
+		$smtp		= new \CeusMedia\Mail\Transport\SMTP( $configSender->get( 'server.host' ) );			//  get SMTP instance
+		$smtp->setPort( $configSender->get( 'server.port' ) );
+		$smtp->setUsername( $configSender->get( 'auth.username' ) );
+		$smtp->setPassword( $configSender->get( 'auth.password' ) );
+
+		$recipient	=  Address::create()
+			->set( $configReceiver->get( 'mailbox.address' ) )
+			->setName( $configReceiver->get( 'mailbox.name' ) );
+		$sender		=  Address::create()
+			->set( $configSender->get( 'mailbox.address' ) )
+			->setName( $configSender->get( 'mailbox.name' ) );
+		$message	= Message::create()
+			->addRecipient( $recipient )
+			->setSender( $sender )
+			->setSubject( $subject )
+			->addText( join( Message::$delimiter, array(
+				'This is a test message for automated testing.'.Message::$delimiter,
+				'Subject:  '.$subject,
+				'Library:  CeusMedia\Mail (https://github.com/CeusMedia/Mail)',
+				'Source:   Transport\SMTP',
+				'Client:   '.Message::create()->getUserAgent(),
+				'Date:     '.date( 'Y-m-d H:i:s' ),
+			) ) );
+		$isMailSent		= $smtp->send( $message );
+		if( !$isMailSent ){
+			$error	= (object) $smtp->getConnectError();
+			$this->markTestSkipped( 'Reason: Sender mailbox connection failed: '.$error->message.' ('.$error->number.')' );
+		}
+
+		/*  --  RECEIVING  --  */
+		$mailbox	= Mailbox::getInstance(
+			$configReceiver->get( 'server.host' ),
+			$configReceiver->get( 'mailbox.address' ),
+			$configReceiver->get( 'auth.password' )
+ 		)->setSecure( TRUE, FALSE );
+		if( !$mailbox->connect( FALSE ) )
+			$this->markTestSkipped( 'Reason: Receiver mailbox connection failed' );
+
+		$loopTime		= 0;
+		$isMailReceived	= FALSE;
+		$searchCriteria	= array( 'SUBJECT "'.$subject.'"' );
+		do{
+			$mailIds	= $mailbox->index( $searchCriteria );
+			if( count( $mailIds ) ){
+				foreach( $mailIds as $mailId )
+					$mailbox->removeMail( $mailId );
+				$isMailReceived	= TRUE;
+				break;
+			}
+			else{
+				sleep( $this->receiveLoopSleep );
+				$loopTime	+= $this->receiveLoopSleep;
+			}
+		} while( $loopTime < $this->receiveLoopTimeout );
+		$this->assertTrue( $isMailReceived );
+	}
+
+	public function _testSend_Virus(){
+		$configSender		= $this->getSenderConfig();
+		$configReceiver		= $this->getReceiverConfig();
+
+		$filePath	= $this->pathTests.'data/test_virus_eicar.txt';
+		$subject	= 'Test-Automation-Message #EICAR-'.\Alg_ID::uuid();
+		$subject	= 'Test-Automation-Message #EICAR-';
+
+		/*  --  SENDING  --  */
+		$smtp		= new \CeusMedia\Mail\Transport\SMTP( $configSender->get( 'server.host' ) );			//  get SMTP instance
+		$smtp->setPort( $configSender->get( 'server.port' ) );
+		$smtp->setUsername( $configSender->get( 'auth.username' ) );
+		$smtp->setPassword( $configSender->get( 'auth.password' ) );
+
+		$recipient	=  Address::create()
+			->set( $configReceiver->get( 'mailbox.address' ) )
+			->setName( $configReceiver->get( 'mailbox.name' ) );
+		$sender		=  Address::create()
+			->set( $configSender->get( 'mailbox.address' ) )
+			->setName( $configSender->get( 'mailbox.name' ) );
+		$message	= Message::create()
+			->addRecipient( $recipient )
+			->setSender( $sender )
+			->setSubject( $subject )
+			->addAttachment( $filePath )
+			->addText( join( Message::$delimiter, array(
+				'This is a test message for automated testing.'.Message::$delimiter,
+				'Subject:  '.$subject,
+				'Library:  CeusMedia\Mail (https://github.com/CeusMedia/Mail)',
+				'Source:   Transport\SMTP',
+				'Client:   '.Message::create()->getUserAgent(),
+				'Date:     '.date( 'Y-m-d H:i:s' ),
+			) ) );
+		$isMailSent		= $smtp->send( $message );
+		if( !$isMailSent ){
+			$error	= (object) $smtp->getConnectError();
+			$this->markTestSkipped( 'Reason: Sender mailbox connection failed: '.$error->message.' ('.$error->number.')' );
+		}
+
+		/*  --  RECEIVING  --  */
+		$receiveLoopSleep	= 10;
+		$receiveLoopTimeout	= 300;
+		$mailbox	= Mailbox::getInstance(
+			$configSender->get( 'server.host' ),
+			$configSender->get( 'mailbox.address' ),
+			$configSender->get( 'auth.password' )
+ 		)->setSecure( TRUE, FALSE );
+		if( !$mailbox->connect( FALSE ) )
+			$this->markTestSkipped( sprintf(
+				'Reason: Receiver mailbox "%s" connection failed',
+				$configSender->get( 'mailbox.address' )
+			) );
+		$loopTime		= 0;
+		$isMailReceived	= FALSE;
+		$searchCriteria	= array( 'SUBJECT "'.$subject.'"' );
+		do{
+			$mailIds	= $mailbox->index( $searchCriteria );
+			if( count( $mailIds ) ){
+				foreach( $mailIds as $mailId )
+					$mailbox->removeMail( $mailId );
+				$isMailReceived	= TRUE;
+				break;
+			}
+			else{
+				sleep( $receiveLoopSleep );
+				$loopTime	+= $receiveLoopSleep;
+			}
+		} while( $loopTime < $receiveLoopTimeout );
+		$this->assertTrue( !$isMailReceived );
 	}
 }
 
