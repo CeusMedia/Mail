5bfa912cda78a3124ed7313666fb2d6527a7b2a3
kriss0r <kriss@ceusmedia.de>
2019-11-01 05:33:40 +0100

    Improve CLI parse demo.

--- demo/cli/parse/parse.php
+++ demo/cli/parse/parse.php
@@ -4,14 +4,24 @@ require_once dirname( __DIR__ ).'/_bootstrap.php';
 use \CeusMedia\Mail\Message;
 use \CeusMedia\Mail\Message\Parser;
 use \CeusMedia\Mail\Message\Part\Text;
+use \CeusMedia\Mail\Message\Part\Attachment;
 
-$content	= \FS_File_Reader::load( __DIR__."/mail.txt" );
+$mailFile		= "mail.txt";
+
+$content	= \FS_File_Reader::load( __DIR__."/".$mailFile );
 $message	= Parser::create()->parse( $content );
-foreach( $message->getParts() as $part ){
-	remark( "Part: ".get_class( $part ) );
+foreach( $message->getParts() as $nr => $part ){
+	remark( "Part #".($nr + 1) );
 	if( $part instanceof Text ){
+		remark( " - Class:    ".get_class( $part ) );
 		remark( " - Encoding: ".$part->getEncoding() );
-		print_m( $part->getContent() );
+		remark( " - Content:  ".$part->getContent() );
+	}
+	else if( $part instanceof Attachment ){
+		remark( " - Class:    ".get_class( $part ) );
+		remark( " - Encoding: ".$part->getEncoding() );
+		remark( " - MimeType: ".$part->getMimeType() );
+		remark( " - Content:  ".strlen( $part->getContent() ).' Bytes' );
 	}
 }
 print( PHP_EOL );
