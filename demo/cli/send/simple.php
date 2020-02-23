30576f28cac1241cee8d0d3a65880f0349e8ecf8
kriss0r <kriss@ceusmedia.de>
2020-02-23 01:18:19 +0100

    Update cli send demo.

--- demo/cli/send/simple.php
+++ demo/cli/send/simple.php
@@ -12,12 +12,17 @@ if( getEnv( 'HTTP_HOST' ) )
 $smtp		= (object) $config->getAll( 'SMTP_' );
 $sending	= (object) $config->getAll( 'sending_' );
 
-SMTP::getInstance( $smtp->host, $smtp->port )
-	->setAuth( $smtp->username, $smtp->password )
-	->setVerbose( $verbose )
-	->send( Message::getInstance()
-		->setSubject( sprintf( $sending->subject, uniqid() ) )
-		->setSender( $sending->senderAddress, $sending->senderName )
-		->addRecipient( $sending->receiverAddress, $sending->receiverName )
-		->addText( sprintf( $sending->body, time() ), "UTF-8", "quoted-printable" )
-	);
+try{
+	SMTP::getInstance( $smtp->host, $smtp->port )
+		->setAuth( $smtp->username, $smtp->password )
+		->setVerbose( $verbose )
+		->send( Message::getInstance()
+			->setSubject( sprintf( $sending->subject, uniqid() ) )
+			->setSender( $sending->senderAddress, $sending->senderName )
+			->addRecipient( $sending->receiverAddress, $sending->receiverName )
+			->addText( sprintf( $sending->body, time() ), "UTF-8", "quoted-printable" )
+		);
+}
+catch( Exception $e ){
+	CLI::out( $e->getMessage() );
+}
