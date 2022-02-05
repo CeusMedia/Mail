47e8a7636460074ca5a981fb9c597e30c53200cc
kriss0r <kriss@ceusmedia.de>
2022-02-05 02:04:15 +0100

    Update tests.

--- test/unit/Message/PartTest.php
+++ test/unit/Message/PartTest.php
@@ -9,6 +9,11 @@
 namespace CeusMedia\Mail\Test\Unit\Message;
 
 use CeusMedia\Mail\Message\Part;
+use CeusMedia\Mail\Message\Part\Attachment as AttachmentPart;
+use CeusMedia\Mail\Message\Part\HTML as HtmlPart;
+use CeusMedia\Mail\Message\Part\InlineImage as InlineImagePart;
+use CeusMedia\Mail\Message\Part\Mail as MailPart;
+use CeusMedia\Mail\Message\Part\Text as TextPart;
 use CeusMedia\Mail\Message\Header\Section as HeaderSection;
 use CeusMedia\Mail\Test\TestCase;
 
@@ -104,6 +109,136 @@ class PartTest extends TestCase
 		$part		= new Message_Part();
 		$part->setMimeType( $mimeType );
 		$this->assertEquals( $mimeType, $part->getMimeType() );
+	}
+
+	/**
+	 *	@covers		::getType
+	 */
+	public function testGetType()
+	{
+		$parts	= [
+			Part::TYPE_ATTACHMENT	=> new AttachmentPart(),
+			Part::TYPE_INLINE_IMAGE	=> new InlineImagePart( 'test' ),
+			Part::TYPE_HTML			=> new HtmlPart( 'test' ),
+			Part::TYPE_TEXT			=> new TextPart( 'test' ),
+			Part::TYPE_MAIL			=> new MailPart( 'test' ),
+		];
+		foreach( $parts as $type => $part ){
+			$this->assertEquals( $type, $part->getType() );
+			$this->assertEquals( $type, $part->getType( TRUE ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isOfType
+	 */
+	public function testIsOfType()
+	{
+		$parts	= [
+			Part::TYPE_ATTACHMENT	=> new AttachmentPart(),
+			Part::TYPE_INLINE_IMAGE	=> new InlineImagePart( 'test' ),
+			Part::TYPE_HTML			=> new HtmlPart( 'test' ),
+			Part::TYPE_TEXT			=> new TextPart( 'test' ),
+			Part::TYPE_MAIL			=> new MailPart( 'test' ),
+		];
+		foreach( $parts as $type => $part ){
+			$this->assertTrue( $part->isOfType( $type ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isAttachment
+	 */
+	public function testIsAttachment()
+	{
+		$part	= new AttachmentPart();
+		$this->assertTrue( $part->isAttachment( $part ) );
+
+		$parts	= [
+			new HtmlPart( 'test' ),
+			new InlineImagePart( 'test' ),
+			new TextPart( 'test' ),
+			new MailPart( 'test' ),
+		];
+		foreach( $parts as $part ){
+			$this->assertFalse( $part->isAttachment( $part ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isInlineImage
+	 */
+	public function testIsInlineImage()
+	{
+		$part	= new InlineImagePart( 'test' );
+		$this->assertTrue( $part->isInlineImage( $part ) );
+
+		$parts	= [
+			new AttachmentPart(),
+			new HtmlPart( 'test' ),
+			new TextPart( 'test' ),
+			new MailPart( 'test' ),
+		];
+		foreach( $parts as $part ){
+			$this->assertFalse( $part->isInlineImage( $part ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isHTML
+	 */
+	public function testIsHTML()
+	{
+		$part	= new HtmlPart( 'test' );
+		$this->assertTrue( $part->isHTML( $part ) );
+
+		$parts	= [
+			new AttachmentPart(),
+			new InlineImagePart( 'test' ),
+			new TextPart( 'test' ),
+			new MailPart( 'test' ),
+		];
+		foreach( $parts as $part ){
+			$this->assertFalse( $part->isHTML( $part ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isMail
+	 */
+	public function testIsMail()
+	{
+		$part	= new MailPart( 'test' );
+		$this->assertTrue( $part->isMail( $part ) );
+
+		$parts	= [
+			new AttachmentPart(),
+			new InlineImagePart( 'test' ),
+			new HtmlPart( 'test' ),
+			new TextPart( 'test' ),
+		];
+		foreach( $parts as $part ){
+			$this->assertFalse( $part->isMail( $part ) );
+		}
+	}
+
+	/**
+	 *	@covers		::isText
+	 */
+	public function testIsText()
+	{
+		$part	= new TextPart( 'test' );
+		$this->assertTrue( $part->isText( $part ) );
+
+		$parts	= [
+			new AttachmentPart(),
+			new InlineImagePart( 'test' ),
+			new HtmlPart( 'test' ),
+			new MailPart( 'test' ),
+		];
+		foreach( $parts as $part ){
+			$this->assertFalse( $part->isText( $part ) );
+		}
 	}
 }
 class Message_Part extends Part
