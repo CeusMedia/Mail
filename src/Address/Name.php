298cdc8e9168099b410b8f4506a6c9a83cdbc3ab
kriss0r <kriss@ceusmedia.de>
2022-02-21 14:14:57 +0100

    Turn address name into data object.

--- src/Address/Name.php
+++ src/Address/Name.php
@@ -46,51 +46,144 @@ use function preg_split;
  */
 class Name
 {
+	/** @var		string|NULL		$firstname */
+	protected $firstname;
+
+	/** @var		string|NULL		$fullname */
+	protected $fullname;
+
+	/** @var		string|NULL		$surname */
+	protected $surname;
+
 	/**
-	 *	...
+	 *	Constructor.
+	 *	@access		public
+	 *	@param		string|NULL		$fullname		Fullname, optional
+	 *	@param		string|NULL		$surname		Surname, optional
+	 *	@param		string|NULL		$firstname		Firstname, optional
+	 *	@return		void
+	 */
+	public function __construct( string $fullname = NULL, string $surname = NULL, string $firstname = NULL )
+	{
+		if( NULL !== $fullname )
+			$this->setFullname( $fullname );
+		if( NULL !== $surname )
+			$this->setSurname( $surname );
+		if( NULL !== $firstname )
+			$this->setFirstname( $firstname );
+	}
+
+	/**
+	 *	Tries to extract firstname and surname for a fullname.
 	 *	@access		public
 	 *	@static
-	 *	@param		array		$list
-	 *	@return		array
+	 *	@param		Name		$name
+	 *	@return		Name
 	 */
-	public static function splitNameParts( array $list ): array
+	public static function splitNameParts( Name $name ): Name
 	{
-		foreach( $list as $nr => $entry ){
-			$fullname	= trim( $entry['fullname'] );
-			$fullname	= preg_replace( '/ +/', ' ', $fullname );
+		$fullname	= trim( $name->getFullname() ?? '' );
+		$fullname	= preg_replace( '/ +/', ' ', $fullname );
+		if( NULL !== $fullname ){
 			if( 1 === preg_match( "/ +/", $fullname ) ){
-				$list[$nr]['fullname']	= $fullname;
+				$name->setFullname( $fullname );
 				$parts	= preg_split( "/ +/", $fullname );
-				if( $parts[0] === strtoupper( $parts[0] ) ){
-					$surname	= array_shift( $parts );
-					$list[$nr]['surname']	= ucfirst( strtolower( $surname ) );
-					$list[$nr]['firstname']	= join( ' ', $parts );
-				}
-				else {
-					$list[$nr]['surname']	= array_pop( $parts );
-					$list[$nr]['firstname']	= join( ' ', $parts );
+				if( FALSE !== $parts && 0 !== count( $parts ) ){
+					if( $parts[0] === strtoupper( $parts[0] ) ){
+						$surname	= array_shift( $parts );
+						$name->setSurname( ucfirst( strtolower( $surname ) ) );
+						$name->setFirstname( join( ' ', $parts ) );
+					}
+					else {
+						$name->setSurname( array_pop( $parts ) );
+						$name->setFirstname( join( ' ', $parts ) );
+					}
 				}
 			}
 		}
-		return $list;
+		return $name;
 	}
 
 	/**
 	 *	...
 	 *	@access		public
 	 *	@static
-	 *	@param		array		$list
-	 *	@return		array
+	 *	@param		Name		$name
+	 *	@return		Name
 	 */
-	public static function swapCommaSeparatedNameParts( array $list ): array
+	public static function swapCommaSeparatedNameParts( Name $name ): Name
 	{
-		foreach( $list as $nr => $entry ){
-			if( 1 === preg_match( "/, +/", $entry['fullname'] ) ){
-				$parts	= preg_split( "/, +/", $entry['fullname'], 2 );
-				if( FALSE !== $parts )
-					$list[$nr]['fullname']	= join( ' ', array_reverse( $parts ) );
-			}
+		if( 1 === preg_match( "/, +/", $name->getFullname() ?? '' ) ){
+			$parts	= preg_split( "/, +/", $name->getFullname() ?? '', 2 );
+			if( FALSE !== $parts )
+				$name->setFullname( join( ' ', array_reverse( $parts ) ) );
 		}
-		return $list;
+		return $name;
+	}
+
+	/**
+	 *	Returns firstname.
+	 *	@access		public
+	 *	@return		string|NULL
+	 */
+	public function getFirstname(): ?string
+	{
+		return $this->firstname;
+	}
+
+	/**
+	 *	Returns fullname.
+	 *	@access		public
+	 *	@return		string|NULL
+	 */
+	public function getFullname(): ?string
+	{
+		return $this->fullname;
+	}
+
+	/**
+	 *	Returns surname.
+	 *	@access		public
+	 *	@return		string|NULL
+	 */
+	public function getSurname(): ?string
+	{
+		return $this->surname;
+	}
+
+	/**
+	 *	Sets firstname.
+	 *	@access		public
+	 *	@param		string		$firstname		Firstname
+	 *	@return		self
+	 */
+	public function setFirstname( string $firstname ): self
+	{
+		$this->firstname	= $firstname;
+		return $this;
+	}
+
+	/**
+	 *	Sets fullname.
+	 *	@access		public
+	 *	@param		string		$fullname		Fullname
+	 *	@return		self
+	 */
+	public function setFullname( string $fullname ): self
+	{
+		$this->fullname	= $fullname;
+		return $this;
+	}
+
+	/**
+	 *	Sets surname.
+	 *	@access		public
+	 *	@param		string		$surname		Surname
+	 *	@return		self
+	 */
+	public function setSurname( string $surname ): self
+	{
+		$this->surname	= $surname;
+		return $this;
 	}
 }
