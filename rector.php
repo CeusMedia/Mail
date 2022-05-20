bbcca97bde54b1eefc5fc8e0b16604e1ccbf2ca8
kriss0r <kriss@ceusmedia.de>
2022-05-20 14:41:09 +0200

    Update rector library and config.

--- rector.php
+++ rector.php
@@ -3,45 +3,29 @@ declare(strict_types=1);
 
 namespace CeusMedia\Mail;
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\FuncCall\AddPregQuoteDelimiterRector;
+use Rector\CodeQuality\Rector\Ternary\ArrayKeyExistsTernaryThenValueToCoalescingRector;
+
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void
+
+return static function (RectorConfig $rectorConfig): void
 {
-	// get parameters
-	$parameters = $containerConfigurator->parameters();
-	$parameters->set(Option::PATHS, [
-		__DIR__ . '/src'
+	$rectorConfig->paths([
+		__DIR__ . '/src',
 	]);
 
-	// Path to phpstan with extensions, that PHPStan in Rector uses to determine types
-	$parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan.neon');
-
-	$targetPhpVersion	= PhpVersion::PHP_74;
-//	$targetPhpVersion	= PhpVersion::PHP_80;
-//	$targetPhpVersion	= PhpVersion::PHP_81;
-
-	$parameters->set(Option::PHP_VERSION_FEATURES, $targetPhpVersion);
-
-	// Define what rule sets will be applied
-//    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
-//	$containerConfigurator->import(SetList::PHP_55);
-//	$containerConfigurator->import(SetList::PHP_56);
-	$containerConfigurator->import(SetList::PHP_70);
-	$containerConfigurator->import(SetList::PHP_71);
-	$containerConfigurator->import(SetList::PHP_73);
-	$containerConfigurator->import(SetList::PHP_74);
-//	$containerConfigurator->import(SetList::PHP_80);
-//	$containerConfigurator->import(SetList::PHP_81);
-
-
-	// get services (needed for register a single rule)
-	// $services = $containerConfigurator->services();
-
 	// register a single rule
-	// $services->set(TypedPropertyRector::class);
+//	$rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+	$rectorConfig->rule(AddPregQuoteDelimiterRector::class);
+	$rectorConfig->rule(ArrayKeyExistsTernaryThenValueToCoalescingRector::class);
+
+	// define sets of rules
+	$rectorConfig->sets([
+		LevelSetList::UP_TO_PHP_74,
+//		SetList::DEAD_CODE,
+	]);
 };
